0221bab6b156bc9be794bcbfbd08980811d0127a
John Doe <idi.qilimi6@gmail.com>
2023-02-23 02:47:11 -0800

    done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,8 +3,8 @@
 matrix_shape = __import__('2-size_me_please').matrix_shape
 def matrix_shape(matrix):
      mat1=([[1, 2], [3, 4]])
-      num_rows=len(mat1)
-       num_cols=len(mat1)
+     num_rows=len(mat1)
+     num_cols=len(mat1)
 def matrix_shape(matrix)
     matrix_shape=[]
         while (type(matrix) is list):
