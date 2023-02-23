e72ae20e1a137319409db06fcf725df952bf534a
John Doe <idi.qilimi6@gmail.com>
2023-02-23 04:31:48 -0800

    done

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    matrix_shape=[]
+    matrix_shape = []
     while (type(matrix) is list):
         matrix_shape.append(len(matrix))
         matrix = matrix[0]
