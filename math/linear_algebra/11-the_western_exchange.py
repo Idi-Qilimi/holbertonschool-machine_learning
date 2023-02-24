38cff6573dcbd5aae92c1889ca2e76e805fca874
John Doe <idi.qilimi6@gmail.com>
2023-02-24 05:26:37 -0800

    done

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Transpose matrix np"""
+
+
+import numpy as np
+"""Transpose matrix np"""
+
+
+def np_transpose(matrix):
+    result = np.array(matrix)
+    r = result.transpose
+    return r
