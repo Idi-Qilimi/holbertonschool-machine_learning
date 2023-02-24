3f7bfb5f9c1451c35343de29a9ac6b1dc7c3b290
John Doe <idi.qilimi6@gmail.com>
2023-02-24 03:53:13 -0800

    done

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -2,7 +2,7 @@
 """Concatenate matrices along axis"""
 
 
-def cat_matrices2D(mat1, mat2, axis = 0):
+def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenate matrices along axis"""
     new_matrix = []
     if axis == 0:
@@ -12,5 +12,5 @@ def cat_matrices2D(mat1, mat2, axis = 0):
     if axis == 1:
         if len(mat1) != len(mat2):
             return None
-        new_matrix = [mat1[i]+ mat2[i] for i in range(len(mat1))]
+        new_matrix = [mat1[i] + mat2[i] for i in range(len(mat1))]
     return new_matrix
