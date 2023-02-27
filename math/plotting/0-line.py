1af399d2442bdc5187a5c4d2b04d18fd71f1e149
John Doe <idi.qilimi6@gmail.com>
2023-02-27 12:10:24 -0800

    done

---
+++ math/plotting/0-line.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Line graph"""
+
+
+import numpy as np
+"""Line graph"""
+
+
+import matplotlib.pyplot as plt
+"""Line graph"""
+
+
+y = np.arange(0, 11) ** 3
+
+plt.plot(y, 'r')
