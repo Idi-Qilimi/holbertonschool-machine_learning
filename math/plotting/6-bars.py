25d805bfcf9b729b1fcc88e2bc3e38098cc9cb3e
John Doe <idi.qilimi6@gmail.com>
2023-02-28 15:05:15 -0800

    done

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -12,5 +12,18 @@ import matplotlib.pyplot as plt
 
 np.random.seed(5)
 fruit = np.random.randint(0, 20, (4,3))
+y1 = np.array([4, 15, 16])
+y2 = np.array([5, 15, 8])
+y3 = np.array([8, 4, 7])
+y4 = np.array([16, 15, 7])
 
-# your code here
+
+categories = ['Farrah', 'Fred', 'Felicia']
+
+
+plt.bar(categories, y1, width=0.5, color='r')
+plt.bar(categories, y2, width=0.5, bottom=y1, color='yellow')
+plt.bar(categories, y3, width=0.5, bottom=y1+y2, color='#ff8000')
+plt.bar(categories, y4, width=0.5, bottom=y1+y2+y3, color='#ffe5b4')
+plt.ylim(0, 80)
+plt.legend(["apples", "bananas", "oranges", "peaches"])
