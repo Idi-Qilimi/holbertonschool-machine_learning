ccb94ec65c061ee71f3ecd790fdd742dc73262d8
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-15 15:34:12 -0700

    done

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -13,5 +13,5 @@ class Normal:
         if data is not None:
             if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            if len(data) <= 3:
                 raise ValueError("data must contain multiple value")
