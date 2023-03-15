7c3f4871b8d4e4d9003c1852ee7c5bbbde91ba01
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-15 15:31:48 -0700

    done

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -4,16 +4,14 @@
 
 class Normal:
     """class that represents normal distribution"""
-
-
     def __init__(self, data=None, mean=0., stddev=1.):
         """class constructor"""
         self.mean = float(mean)
         self.stddev = float(stddev)
+        if (self.stddev <= 0):
+            raise ValueError("stddev must be a positive value")
         if data is not None:
-            if (self.stddev <= 0):
-                raise ValueError("stddev must be a positive value")
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
+                raise ValueError("data must contain multiple value")
