f395066c82078f23a954159d566f51b42f5ec4b3
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-15 15:25:31 -0700

    done

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -11,7 +11,7 @@ class Normal:
         self.mean = float(mean)
         self.stddev = float(stddev)
         if data is not None:
-            if self.stddev <= 0:
+            if (self.stddev <= 0):
                 raise ValueError("stddev must be a positive value")
             if type(data) is not list:
                 raise TypeError("data must be a list")
