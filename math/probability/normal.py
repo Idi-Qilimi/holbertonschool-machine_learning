6dbd2f0e3727b0299a86a51a5b8fdc8b07d22103
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-16 06:45:55 -0700

    done

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -6,15 +6,25 @@ class Normal:
     """class that represents normal distribution"""
     def __init__(self, data=None, mean=0., stddev=1.):
         """class constructor"""
-        self.mean = float(mean)
-        self.stddev = float(stddev)
-        if (self.stddev <= 0):
-            raise ValueError("stddev must be a positive value")
-        if data is not None:
+        if data is None:
+            if stddev < 1:
+                raise ValueError("stddev must be a positive value")
+            else:
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
-            if len(data) < 2:
+            elif len(data) < 2:
                 raise ValueError("data must contain multiple value")
+            else:
+                mean = float(sum(data) / len(data)
+                self.mean = mean
+                summation = 0
+                for x in data:
+                    summation += (summation / len(data)) ** (1 / 2)
+                stddev = (summation / len(data)) ** (1 / 2)
+                self.stddev = stddev
 
     def z_score(self, x):
         """calculates the z-score of a given x-value"""
@@ -29,3 +39,5 @@ class Normal:
         stddev = self.stddev
         x = (z * stddev) - mean
         return x
+
+    
