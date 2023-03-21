478edd45f3ecc68fe5e89ffd3d5930ed94a66daa
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-21 07:29:39 -0700

    done

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -6,18 +6,34 @@ import numpy as np
 """Convolution Grayscale"""
 
 
-def convolve_grayscale_padding(images, kernel, padding):
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """Convolution Grayscale"""
     m, height, width = images.shape
     kh, kw = kernel.shape
-    ph, pw = padding
-    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
-    ch = height + 2*ph - kh + 1
-    cw = width + 2*pw - kw + 1
-    convoluted = np.zeros(m, ch, cw)
-    for h in range(ch):
-        for w in range(cw):
-            out = np.sum(images[:, h: h+kh, w+kw]*kernel,
-                         axis=1).sum(axis=1)
-            convoluted[:, h, w] = out
-    return convoluted
+    sh, sw = stride
+    if padding == "same":
+        if (kh % 2) == 1 and (kw % 2) == 1:
+            ph = (kh - 1) // 2
+            pw = (kw - 1) // 2
+        else:
+            ph = kh // 2
+            pw = kw // 2
+    elif padding == "valid":
+        ph = 0
+        pw = 0
+    else:
+        ph, pw = padding
+        images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)))
+        ch = ((height + (2*ph) - kh) // sh) + 1
+        cw = ((width + (2*pw) - kw) // sw) + 1
+        convoluted = np.zeros((m, ch, cw))
+        i = 0
+        for h in range(0, height+(2*ph)-kh+1, sh):
+            j = 0
+            for w in range (0, weight+(2*pw)-kh+1, sw):
+                output_conv = np.sum(images[:, h:h+kh, w:w+kw]
+                                     *kernel, axis=1).sum(axis=1)
+                convoluted[:, i, j] = output_conv
+                j += 1
+            i += 1
+            return convoluted
