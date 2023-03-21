63283551cd5098667d0c39eae63bbc6485cfba92
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-21 07:52:30 -0700

    done

---
+++ math/convolutions_and_pooling/6-pool.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""pooling on images"""
+
+
+import numpy as np
+"""pooling on images"""
+
+
+def pool(images, kernel_shape, stride, mode='max'):
+    """pooling on images"""
+    m, height, width, c = images.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    ph = ((height - kh) // sh) + 1
+    pw = ((weight - kw) // sw) + 1
+    pooled = np.zeros((m, ph, pw, c))
+    i = 0
+    for h in range(0, height - kh + 1, sh):
+        j = 0
+        for w in range(0, width - kw + 1, sw):
+            if mode == "max":
+                output = np.max(images[:, h: h+kh, width: width+ kw, :],
+                                axis=(1, 2))
+            if mode == "average":
+                output = np.average(images[:, h: h+kh,
+                                           width: width+ kw, :], axis=(1, 2))
+            pooled[:, i, j] = output
+            j+=1
+        i+=1
+    return pooled
