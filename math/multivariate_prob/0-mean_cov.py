4f759d86722093364f9b77303abdd737fd2b7b9a
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-24 07:51:37 -0700

    done

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -10,9 +10,9 @@ def mean_cov(X):
     """Calculate mean and covariance of data set"""
     n, d = X.shape
     if len(X.shape) < 2:
-        raise TypeError("X must contain multiple data points")
-    if n < 2:
         raise ValueError("X must contain multiple data points")
+    if len(n) < 2:
+        raise TypeError("X must must be a 2D numpy.ndarray")
     mean = np.mean(X, axis=0)
     cov = np.zeros((d, d))
     for i in range(n):
