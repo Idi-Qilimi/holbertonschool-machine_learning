987f182e5c9af0a43dda37e0e4070d37ee21d423
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-30 05:27:47 -0700

    done

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -9,11 +9,11 @@ import numpy as np
 class MultiNormal:
     """Multivariate Normal distribution"""
     def __init__(self, data):
-         """Multivariate Normal distribution"""
-         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
-             raise TypeError("data must be a 2D numpy.ndarray")
-         n, d = data.shape
-         if n < 2:
-             raise ValueError("data must contain multiple data points")
-         self.mean, self.cov = mean_cov(data.T)
-         self.mean = self.mean.reshape(-1, 1)
+        """Multivariate Normal distribution"""
+        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        n, d = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean, self.cov = mean_cov(data.T)
+        self.mean = self.mean.reshape(-1, 1)
