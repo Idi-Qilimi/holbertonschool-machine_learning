1f611913385954070af4acb9dd8bee3200eeafd3
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-03-30 05:21:45 -0700

    done

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""calculates a correlation matrix"""
+
+
+import numpy as np
+"""calculates a correlation matrix"""
+
+
+def correlation(C):
+    """calculates a correlation matrix"""
+    if type(C) is not np.ndarray:
+        raise TypeError("C must be a numpy.ndarray")
+    if (len(C.shape) != 2) or (C.shape[0] != C.shape[1]):
+        raise ValueError("C must be a 2D square matrix")
+    D = np.sqrt(np.diag(C))
+    D_inverse = 1 / np.outer(D, D)
+    corr = D_inverse * C
+    return corr
