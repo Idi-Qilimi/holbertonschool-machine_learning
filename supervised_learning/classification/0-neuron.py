f81bfdd20eb303b122074c28c887c4a19591f1c4
Idi-Qilimi <idi.qilimi6@gmail.com>
2023-04-20 07:43:27 -0700

    done

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+"""Perform binary classification"""
+
+
+import numpy as np
+"""Perform binary classification"""
+
+
 class Neuron:
     """Perform binary classification"""
     def __init__(self, nx):
         if type(nx) is not int:
-            raise TypeError("nx must be integer")
+            raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        self.W = np.random.randn(1,nx)
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
