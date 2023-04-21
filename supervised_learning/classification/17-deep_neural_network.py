ae268b0ab85fc5cce721f25894f8998f87fed95a
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-04-21 20:40:45 +0200

    Update 17-deep_neural_network.py

--- supervised_learning/classification/17-deep_neural_network.py
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -21,12 +21,13 @@ class DeepNeuralNetwork:
             if type(layer) is not int or layer < 0:
                 raise TypeError("layers must be a list of positive integers")
             weights["b{}".format(index)] = np.zeros((layer, 1))
-            weights["W{}".format(index)] = (np.random.randn(layer, previous) * np.sqrt(2 / previous))
+            weights["W{}".format(index)] = (np.random.randn(layer, previous) *
+                                            np.sqrt(2 / previous))
             previous = layer
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = weights
-        
+
     @property
     def L(self):
         return (self.__L)
