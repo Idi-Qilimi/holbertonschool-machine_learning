1592b0b71385f3956112c44e14af92679a57e7de
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-04-21 20:17:48 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -10,16 +10,16 @@ class DeepNeuralNetwork:
     """class that represents a deep neural network"""
     def __init__(self, nx, layers):
         if type(nx) is not int:
-            raise TypeError("...")
+            raise TypeError("nx must be an integer")
         if nx < 1:
-            raise ValueError("...")
+            raise ValueError("nx must be a positive integer")
         if type(layers) is not list or len(layers) < 1:
-            raise TyperError("...")
+            raise TyperError("layers must be a list of positive integers")
         weights = {}
         previous = nx
         for index, layer in enumerate(layers, 1):
             if type(layer) is not int or layer < 0:
-                raise TypeError("list of positive integers")
+                raise TypeError("layers must be a list of positive integers")
             weights["b{}".format(index)] = np.zeros((layer, 1))
             weights["W{}".format(index)] = np.random.randn(layer, previous) *
                 np.sqrt(2 / previous)
