8ca6c3f2f6c8a5491276ccc352b74a18d69541b9
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-04-21 20:36:07 +0200

    Update 16-deep_neural_network.py

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -25,6 +25,7 @@ class DeepNeuralNetwork:
             weights["b{}".format(index)] = np.zeros((layer, 1))
             weights["W{}".format(index)] = (np.random.randn(layer, previous) *
                                             np.sqrt(2 / previous))
+            previous = layer
         self.L = len(layers)
         self.cache = {}
         self.weights = weights
