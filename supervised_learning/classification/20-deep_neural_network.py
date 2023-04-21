bfefa17b9f986da31a5cccc4604d48c621a57e43
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-04-21 20:49:43 +0200

    Update 20-deep_neural_network.py

--- supervised_learning/classification/20-deep_neural_network.py
+++ supervised_learning/classification/20-deep_neural_network.py
@@ -59,6 +59,7 @@ class DeepNeuralNetwork:
         return cost
     
     def evaluate(self, X, Y):
+        """evaluate deep neural network"""
         A, cache = self.forward_prop(X)
         cost = self.cost(Y, A)
         prediction = np.where(A >= 0.5, 1, 0)
