5d3a01b310fe4fec9271d7c9e2f3fa03a629f4a7
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-04 16:52:33 +0200

    Update 10-Adam.py

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -9,8 +9,8 @@ import tensorflow as tf
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """Upgrade Adam"""
     optimizer = tf.keras.optimizers.Adam(
-        learning_rate=alpha, 
-        beta_1=beta1, 
-        beta_2=beta2, 
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
         epsilon=epsilon)
     return optimizer.minimize(loss)
