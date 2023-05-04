1e76703311bf22716b43f13c8cf542fb0ad6c5b0
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-04 16:47:29 +0200

    Update 8-RMSProp.py

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """Upgrade RMSProp"""
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha,
+    decay=beta2, epsilon=epsilon)
     train_op = optimizer.minimize(loss)
     return train_op
