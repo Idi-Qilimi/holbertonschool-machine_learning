0d24e339363706d83bc854b5e33857ff37f940c0
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-04 16:54:47 +0200

    Update 12-learning_rate_decay.py

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -8,5 +8,6 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Learning rate upgrade"""
-    alpha_new = tf.divide(alpha, tf.pow(1 + decay_rate * tf.floor_div(global_step, decay_step), 1))
+    alpha_new = tf.divide(alpha, tf.pow(
+    1 + decay_rate * tf.floor_div(global_step, decay_step), 1))
     return alpha_new
