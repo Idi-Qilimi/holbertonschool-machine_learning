30a23478e0c3aab4b141f54e4fbe5dfa3433ab5f
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-08 18:22:21 +0200

    Update 4-f1_score.py

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python3
+"""F1 score"""
+
+
+import numpy as np
+"""F1 score"""
+
+
 def f1_score(confusion):
+    """F1 score"""
     p = precision(confusion)
     r = sensitivity(confusion)
     f1 = 2 * r * p / (r + p)
