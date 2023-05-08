bfd8ee66f0b696e440ce869bd8b63d90e51e4636
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-08 18:24:14 +0200

    Update 4-f1_score.py

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -6,6 +6,14 @@ import numpy as np
 """F1 score"""
 
 
+sensitivity = __import__('1-sensitivity').sensitivity
+"""F1 score"""
+
+
+precision = __import__('2-precision').precision
+"""F1 score"""
+
+
 def f1_score(confusion):
     """F1 score"""
     p = precision(confusion)
