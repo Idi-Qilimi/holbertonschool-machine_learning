cbb43ad88cc2472db0b9a3430a233a8be29fc299
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-08 18:20:13 +0200

    Update 2-precision.py

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python3
+"""Precision"""
+
+
+import numpy as np
+"""Precision"""
+
+
 def precision(confusion):
+    """Precision"""
     tp = np.diag(confusion)
     fp = np.sum(confusion, axis=0) - tp
     precision = tp / (tp + fp) 
