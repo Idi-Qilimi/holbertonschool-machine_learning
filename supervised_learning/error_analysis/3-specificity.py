7f741222319f4e1ff4c1959764c953d5adc7a187
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-08 18:21:19 +0200

    Update 3-specificity.py

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python3
+"""Specificity"""
+
+
+import numpy as np
+"""Specificity"""
+
+
 def specificity(confusion):
+    """Specificity"""
     tp = np.diag(confusion)
     fp = confusion.sum(axis=0) - tp
     fn = confusion.sum(axis=1) - tp
