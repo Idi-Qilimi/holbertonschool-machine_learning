3710c917532e5f6c2c5bdb7d31a37008eebfeb12
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-05-19 16:39:49 +0200

    Update 1-input.py

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -11,7 +11,6 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     reg_l2 = K.regularizers.l2(lambtha)
     inputs = K.Input(shape=(nx,))
     X = inputs
-    
     for i in range(len(layers)):
         if i != 0:
             X = K.layers.Dropout(1 - keep_prob)(X)
