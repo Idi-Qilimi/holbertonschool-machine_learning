397cbc72ab0c007c6956c74b593161618d3b1aeb
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-06-15 11:31:41 +0200

    Update 4-lenet5.py

--- supervised_learning/cnn/4-lenet5.py
+++ supervised_learning/cnn/4-lenet5.py
@@ -9,18 +9,18 @@ import tensorflow as tf
 def lenet5(x, y):
     """Le Net 5"""
     conv1 = tf.keras.layers.Conv2D(filters=6, kernel_size=5,
-                                   padding='same', 
-                                   activation='relu', 
+                                   padding='same',
+                                   activation='relu',
                                    kernel_initializer='he_normal')(x)
     pool1 = tf.keras.layers.MaxPooling2D(
         pool_size=2, strides=2)(conv1)
     conv2 = tf.keras.layers.Conv2D(filters=16, kernel_size=5, padding='valid',
-                                   activation='relu', 
+                                   activation='relu',
                                    kernel_initializer='he_normal')(pool1)
     pool2 = tf.keras.layers.MaxPooling2D(pool_size=2, strides=2)(conv2)
     flatten = tf.keras.layers.Flatten()(pool2)
-    fc1 = tf.keras.layers.Dense(120, 
-                                activation='relu', 
+    fc1 = tf.keras.layers.Dense(120,
+                                activation='relu',
                                 kernel_initializer='he_normal')(flatten)
     fc2 = tf.keras.layers.Dense(84, activation='relu',
                                 kernel_initializer='he_normal')(fc1)
