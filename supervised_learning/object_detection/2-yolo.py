001a4532f5951b23cc2b3b6f5c81e3cbd269aa0c
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-06-22 17:16:51 +0200

    Update 2-yolo.py

--- supervised_learning/object_detection/2-yolo.py
+++ supervised_learning/object_detection/2-yolo.py
@@ -8,7 +8,7 @@ class Yolo:
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = load_model(model_path)
         with open(classes_path) as f:
-            self.class_names = [line.strip for line in f]
+            self.class_names = [line.strip() for line in f]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
