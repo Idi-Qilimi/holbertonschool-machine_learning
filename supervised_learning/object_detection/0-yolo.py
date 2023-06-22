f2fac46f94b387674f3fc9f689031f0f3ce70ee9
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-06-22 16:00:26 +0200

    Create 0-yolo.py

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Initialize Yolo"""
+
+
+class Yolo:
+    """Initialize Yolo"""
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        self.model = load_model(model_path)
+        with open(classes_path) as f:
+            self.class_names = [line.strip for line in f]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
