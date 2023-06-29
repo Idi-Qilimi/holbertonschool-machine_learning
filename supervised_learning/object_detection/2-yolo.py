d414763ac35986da555a5c5695ea0a25c4ef91ce
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-06-29 20:44:58 +0200

    Update 2-yolo.py

--- supervised_learning/object_detection/2-yolo.py
+++ supervised_learning/object_detection/2-yolo.py
@@ -1,69 +1,115 @@
 #!/usr/bin/env python3
-"""Initialize Yolo"""
-from tensorflow.keras.models import load_model
+"""
+Defines class Yolo that uses the Yolo v3 algorithm to perform object detection
+"""
+import tensorflow.keras as K
 import numpy as np
-import glob
-import os
 
 
 class Yolo:
-    """Initialize Yolo"""
+    """
+    Class that uses Yolo v3 algorithm to perform object detection
+    public instance attributes:
+        model: the Darknet Keras model
+        class_names: list of all the class names for the model
+        class_t: the box score threshold for the initial filtering step
+        nms_t: the IOU threshold for non-max suppression
+        anchors: the anchor boxes
+    """
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        self.model = load_model(model_path)
-        with open(classes_path) as f:
-            self.class_names = [line.strip() for line in f]
+        """
+        Yolo class constructor
+        parameters:
+            model_path: the path to where a Darknet Keras model is stored
+            classes_path: the path to where the list of class names
+                used for the Darknet model can be found
+            class_t: represents the box score threshold for
+                the initial filtering step
+            nms_t: represents the IOU threshold for non-max suppression
+            anchors :contains all the anchor boxes
+        """
+        self.model = K.models.load_model(model_path)
+        with open(classes_path, 'r') as f:
+            lines = f.readlines()
+            self.class_names = []
+            for name in lines:
+                self.class_names.append(name[:-1])
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
 
+    @staticmethod
+    def sigmoid(x):
+        """
+        Returns the output after passing through Sigmoid function
+        output will be between 0 and 1
+        """
+        return (1. / (1. + np.exp(-x)))
+
     def process_outputs(self, outputs, image_size):
-        """Process Outputs Yolo"""
+        """
+        Processes the outputs
+        """
         boxes = []
         box_confidences = []
         box_class_probs = []
-        for i in range(len(outputs)):
-            boxes.append(output[i][..., 4])
-            box_confidences.append(1 / (1 + np.exp(-outputs[i][..., 4:5])))
-            box_class_probs.append(1 / (1 + np.exp(-outputs[i][..., 5:])))
-        image_height, image_width = image_size
-        for i in range(len(boxes)):
-            grid_width = outputs[i].shape[0]
-            grid_height = outputs[i].shape[1]
-            anchor_boxes = outputs[i].shape[2]
-            for cy in range(grid_height):
-                for cx in range(grid_width):
-                    for b in range(anchor_boxes):
-                        tx, ty, tw, th = boxes[i][cy, cx, b]
-                        pw, ph = self.anchors[i][b]
-                        bx = (1 / (1 + np.exp(-tx))) + cx
-                        by = (1 / (1 + np.exp(-ty))) + cy
-                        bw = pw * np.exp(tw)
-                        bh = ph * np.exp(th)
-                        bx /= grid_width
-                        by /= grid_height
-                        bw /= self.model.input.shape[1]
-                        bh /= self.model.input.shape[1]
-                        x1 = (bx - (bw / 2)) * image_width
-                        y1 = (by - (bh / 2)) * image_height
-                        x2 = (bx + (bw / 2)) * image_width
-                        y2 = (by + (bh / 2)) * image_height
-                        boxes[i][cy, cx, b] = [x1, y1, x2, y2]
+        for i, output in enumerate(outputs):
+            anchors = self.anchors[i]
+            grid_height, grid_width = output.shape[:2]
+
+            t_xy = output[..., :2]
+            t_wh = output[..., 2:4]
+
+            sigmoid_conf = self.sigmoid(output[..., 4])
+            sigmoid_prob = self.sigmoid(output[..., 5:])
+
+            box_conf = np.expand_dims(sigmoid_conf, axis=-1)
+            box_class_prob = sigmoid_prob
+
+            box_confidences.append(box_conf)
+            box_class_probs.append(box_class_prob)
+
+            b_wh = anchors * np.exp(t_wh)
+            b_wh /= self.model.inputs[0].shape.as_list()[1:3]
+
+            grid = np.tile(np.indices((grid_width, grid_height)).T,
+                           anchors.shape[0]).reshape(
+                               (grid_height, grid_width) + anchors.shape)
+
+            b_xy = (self.sigmoid(t_xy) + grid) / [grid_width, grid_height]
+
+            b_xy1 = b_xy - (b_wh / 2)
+            b_xy2 = b_xy + (b_wh / 2)
+            box = np.concatenate((b_xy1, b_xy2), axis=-1)
+            box *= np.tile(np.flip(image_size, axis=0), 2)
+
+            boxes.append(box)
         return (boxes, box_confidences, box_class_probs)
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
-        """Filter boxes"""
-        filtered_boxes, box_classes, box_scores = None, [], []
-        for i in range(len(boxes)):
-            cur_box_score = box_confidences[i] * box_class_probs[i]
-            cur_box_class = np.argmax(cur_box_score, axis=-1)
-            cur_box_score = np.max(cur_box_score, axis=-1)
-            mask = cur_box_score >= self.class_t
-            if filtered_boxes is None:
-                filtered_boxes = boxes[i][mask]
-                box_scores = cur_box_score[mask]
-                box_classes = cur_box_class[mask]
-            else:
-                filtered_boxes = np.concatenate((filtered_boxes, boxes[i][mask]), axis =0)
-                box_classes = np.concatenate((box_classes, cur_box_class[i][mask]), axis=0)
-                box_scores = np.concatenate((box_scores, cur_box_score[mask]), axis=0)
-        return(filtered_boxes, box_classes, box_scores)
+        """
+        Determines filtered bounding boxes from processed outputs
+        """
+        filtered_boxes = []
+        box_classes = []
+        box_scores = []
+
+        for i, b in enumerate(boxes):
+            box_conf = box_confidences[i]
+            box_class_prob = box_class_probs[i]
+
+            box_score = box_conf * box_class_prob
+
+            box_class = np.argmax(box_score, axis=-1)
+            box_class_score = np.max(box_score, axis=-1)
+
+            index = np.where(box_class_score >= self.class_t)
+
+            filtered_boxes.append(b[index])
+            box_classes.append(box_class[index])
+            box_scores.append(box_class_score[index])
+
+        filtered_boxes = np.concatenate(filtered_boxes)
+        box_classes = np.concatenate(box_classes)
+        box_scores = np.concatenate(box_scores)
+        return (filtered_boxes, box_classes, box_scores)
