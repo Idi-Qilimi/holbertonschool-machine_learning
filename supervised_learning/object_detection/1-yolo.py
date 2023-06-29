cbc5659a3397e6ce9c38ff43b699523fcfcd4f10
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-06-29 10:49:36 +0200

    Update 1-yolo.py

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""Initialize Yolo"""
+"""Task 0. Initialize Yolo"""
 from tensorflow.keras.models import load_model
 import numpy as np
-import glob
-import os
 
 
 class Yolo:
-    """Initialize Yolo"""
+   
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         self.model = load_model(model_path)
         with open(classes_path) as f:
@@ -17,22 +15,27 @@ class Yolo:
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """Process Outputs Yolo"""
+        
         boxes = []
         box_confidences = []
         box_class_probs = []
+        
         for i in range(len(outputs)):
-            boxes.append(output[i][..., 4])
+            boxes.append(outputs[i][..., :4])
             box_confidences.append(1 / (1 + np.exp(-outputs[i][..., 4:5])))
             box_class_probs.append(1 / (1 + np.exp(-outputs[i][..., 5:])))
         image_height, image_width = image_size
+        
         for i in range(len(boxes)):
-            grid_width = outputs[i].shape[0]
-            grid_height = outputs[i].shape[1]
+            
+            grid_width = outputs[i].shape[1]
+            grid_height = outputs[i].shape[0]
             anchor_boxes = outputs[i].shape[2]
+            
             for cy in range(grid_height):
                 for cx in range(grid_width):
                     for b in range(anchor_boxes):
+                        
                         tx, ty, tw, th = boxes[i][cy, cx, b]
                         pw, ph = self.anchors[i][b]
                         bx = (1 / (1 + np.exp(-tx))) + cx
@@ -42,10 +45,11 @@ class Yolo:
                         bx /= grid_width
                         by /= grid_height
                         bw /= self.model.input.shape[1]
-                        bh /= self.model.input.shape[1]
+                        bh /= self.model.input.shape[2]
                         x1 = (bx - (bw / 2)) * image_width
                         y1 = (by - (bh / 2)) * image_height
                         x2 = (bx + (bw / 2)) * image_width
                         y2 = (by + (bh / 2)) * image_height
                         boxes[i][cy, cx, b] = [x1, y1, x2, y2]
+                        
         return (boxes, box_confidences, box_class_probs)
