a8e5c826411f24cd7143bf03a27855782dcb8b20
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-07-06 16:02:19 +0200

    Update 8-bi_rnn.py

--- supervised_learning/RNNs/8-bi_rnn.py
+++ supervised_learning/RNNs/8-bi_rnn.py
@@ -5,16 +5,17 @@ import numpy as np
 
 def bi_rnn(bi_cell, X, h_0, h_t):
     """Forward prop bidirectional"""
-    t, m, i = X.shape
-    h = h_0.shape[1]
-    H = np.zeros((t, m, 2 * h))
-    Y = np.zeros((t, m, bi_cell.by.shape[1]))
-    h_forward = h_0
-    h_backward = h_t
+    H_for = np.zeros((t, m, h))
+    H_back = np.zeros((t, m, h))
+    h_ft = h_0
+    h_bt = h_t
     for step in range(t):
-        x_t = X[step]
-        h_forward = bi_cell.forward(h_forward, x_t)
-        h_backward = bi_cell.backward(h_backward, x_t)
-        H[step] = np.concatenate((h_forward, h_backward), axis=1)
-        Y[step] = bi_cell.output(H[step])
+        x_ft = X[step]
+        x_bt = X[-(step + 1)]
+        h_ft = bi_cell.forward(h_ft, x_ft)
+        h_bt = bi_cell.backward(h_bt, x_bt)
+        H_for[step] = h_ft
+        H_back[-(step + 1)] = h_bt
+    H = np.concatenate((H_for, H_back), axis=-1)
+    Y = bi_cell.output(H)
     return H, Y
