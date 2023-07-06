2d6c33ec42d1608431be162fd64585c61de89e8e
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-07-06 16:00:19 +0200

    Update 4-deep_rnn.py

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -6,20 +6,20 @@ import numpy as np
 def deep_rnn(rnn_cells, X, h_0):
     """Deep RNN"""
     t, m, i = X.shape
-    length = len(rnn_cells)
-    h = h_0.shape[2]
+    l, m, h = h_0.shape
+    o = rnn_cells[-1].by.shape[1]
     H = np.zeros((t + 1, l, m, h))
-    Y = np.zeros((t, m, rnn_cells[-1].by.shape[1]))
+    Y = np.zeros((t, m, o))
     H[0] = h_0
+
     for step in range(t):
-        for layer in range(l):
-            if layer == 0:
-                h_prev = H[step, layer]
-                h_next, y = rnn_cells[layer].forward(h_prev, X[step])
-            else:
-                h_prev = H[step, layer]
-                h_next, y = rnn_cells[layer].forward(
-                            h_prev, H[step, layer - 1])
-            H[step + 1, layer] = h_next
-            Y[step] = y
-    return H[1:], Y
+        h_aux = X[step]
+        for layer in range(len(rnn_cells)):
+            r_cell = rnn_cells[layer]
+            x_t = h_aux
+            h_prev = H[step][layer]
+            h_next, y_next = r_cell.forward(h_prev=h_prev, x_t=x_t)
+            h_aux = h_next
+            H[step + 1][layer] = h_aux
+        Y[step] = y_next
+    return H, Y
