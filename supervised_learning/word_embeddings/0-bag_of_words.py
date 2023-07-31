803bd6906d8cbd5531d57006fccfbf47fe221e0c
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-07-31 12:34:31 +0200

    Update 0-bag_of_words.py

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 """Bag of words"""
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
-import re
 
 
 def bag_of_words(sentences, vocab=None):
-    """Bag of Words"""
-    if vocab is None:
-        vectorizer = CountVectorizer()
-        X = vectorizer.fit_transform(sentences)
-        vocab = vectorizer.get_feature_names_out()
-    vocab = list(set(vocab))
-    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
-    for i, sentence in enumerate(sentences):
-        processed_sentence = re.sub(r'[^\w\s]', '', sentence.lower())
-        words = processed_sentence.split()
-        for word in words:
-            try:
-                idx = vocab.index(word)
-                embeddings[i, idx] += 1
-            except ValueError:
-                pass
-    features = vocab
+    """
+    Creates a bag of words embedding matrix
+    sentences: a list of sentences to analyze
+    vocab: a list of the vocabulary words to use for the analysis
+    """
+    vector = CountVectorizer(vocabulary=vocab)
+    X = vector.fit_transform(sentences)
+    features = vector.get_feature_names()
+    embeddings = X.toarray()
+
     return embeddings, features
