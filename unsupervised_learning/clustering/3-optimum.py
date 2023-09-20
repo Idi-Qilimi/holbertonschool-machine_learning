4f4fff5789877b1f22c01bdb6529939653ac6c87
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-09-20 10:35:54 +0200

    Update 3-optimum.py

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Optimum K method aka inversed elbow"""
-
 import numpy as np
-
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
