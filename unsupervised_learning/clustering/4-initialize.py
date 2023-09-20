c29775aa34bef09750d5da9b46ebf754193c083a
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-09-20 10:40:18 +0200

    Update 4-initialize.py

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """GMM function """
 import numpy as np
+kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
     """
     initializes variables for a Gaussian Mixture Model
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k < 1:
+    if type(k) is not int or k <= 0:
         return None, None, None
 
     n, d = X.shape
