8235ee4e78d85f3d9b416a186459cfd8dd124782
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-10-17 17:07:45 +0200

    Create 0-from_numpy.py

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import string
+
+def from_numpy(array):
+    """
+    Creates a Pandas DataFrame from a numpy.ndarray.
+    """
+    if array.shape[1] > 26:
+        raise ValueError("Input array has more than 26 columns.")
+
+    column_labels = list(string.ascii_uppercase)[:array.shape[1]]
+    df = pd.DataFrame(array, columns=column_labels)
+    return df
