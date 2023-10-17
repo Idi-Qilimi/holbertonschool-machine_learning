290905a47dfdb1eea4121fcc4ac339c881d2cd3d
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-10-17 17:10:47 +0200

    Create 6-flip_switch.py

---
+++ pipeline/pandas/6-flip_switch.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = from_file('../supervised_learning/data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df = df.sort_values(by='Timestamp', ascending=False).T
+
+print(df.tail(8))
