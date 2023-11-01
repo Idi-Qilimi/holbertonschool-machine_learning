36cd0cbc183ce81e3b3efdefbd495529926fd41d
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-11-01 23:30:21 +0100

    Update 16-glam_rock.sql

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,13 +1,17 @@
 -- Calculate the lifespan for each band and select the required columns
 SELECT
     band_name,
-    (CASE
-        WHEN split = 0 THEN 2020 - formed
-        ELSE split - formed
-    END) AS lifespan_until_2020
+    TIMESTAMPDIFF(YEAR, formed, IFNULL(split, CURDATE())) AS lifespan_until_2020
 FROM
     bands
 WHERE
-    band_name IN (SELECT band_name FROM bands WHERE style = 'Glam rock')
+    band_name IN (
+        SELECT
+            band_name
+        FROM
+            bands
+        WHERE
+            style = 'Glam rock'
+    )
 ORDER BY
     lifespan_until_2020 DESC;
