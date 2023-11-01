3247c940201e60977294aeff8607741346f6b9b8
Idi <110457131+Idi-Qilimi@users.noreply.github.com>
2023-11-01 23:28:47 +0100

    Update 16-glam_rock.sql

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,6 +1,13 @@
--- Ranks Glam rock bands by longevity
-SELECT band_name, IF(split IS NULL, YEAR(CURDATE()), split) - formed AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC
-;
+-- Calculate the lifespan for each band and select the required columns
+SELECT
+    band_name,
+    (CASE
+        WHEN split = 0 THEN 2020 - formed
+        ELSE split - formed
+    END) AS lifespan_until_2020
+FROM
+    bands
+WHERE
+    band_name IN (SELECT band_name FROM bands WHERE style = 'Glam rock')
+ORDER BY
+    lifespan_until_2020 DESC;
